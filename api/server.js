fde293ee409b65e54724c277ebacd137a611fcbf
Arjun Thakur <105769952+arjunsinghjadaun@users.noreply.github.com>
2025-07-26 17:14:10 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -1,13 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import router from "../routes/authRoutes.js";
+import cors from "cors";
 
-const app = express();
+const server = express();
+server.use(express.json());
+server.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Express app working!" });
+const PORT = 3000;
+
+server.get("/api/hello", (req, res) => {
+  res.send("Welcome to the E-commerce Backend!");
 });
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello route working!" });
-});
+mongoose
+  .connect(
+    "mongodb+srv://vikaskumar20012001:vikas1234@demo-wd-8.sdcsiw7.mongodb.net/E-commerce"
+  )
 
-export default app;
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+server.use("/api/auth", router);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
