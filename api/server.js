e92939d46399a37ec59656c4d313934440e5d993
Arjun Thakur <105769952+arjunsinghjadaun@users.noreply.github.com>
2025-07-26 16:29:00 +0530

    -make small changes

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.use(express.json());
 server.use(cors());
 
 
-server.get("/", (req, res) => {
+server.get("/hello", (req, res) => {
   res.send("Welcome to the E-commerce Backend!");
 });
 
