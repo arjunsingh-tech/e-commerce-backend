5ab24fae113de00272000f771269883f2e40559c
Arjun Thakur <105769952+arjunsinghjadaun@users.noreply.github.com>
2025-07-26 17:03:33 +0530

    checking

--- api/server.js
+++ api/server.js
@@ -1,33 +1,13 @@
 import express from "express";
-import mongoose from "mongoose";
-import router from "../routes/authRoutes.js"; 
-import cors from "cors";
 
-const server = express();
+const app = express();
 
-server.use(express.json());
-server.use(cors());
-
-
-server.get("/", (req, res) => {
-  res.send("API is working! Welcome to the E-commerce Backend!");
+app.get("/", (req, res) => {
+  res.json({ message: "Express app working!" });
 });
 
-server.get("/hello", (req, res) => {
-  res.send("Welcome to the E-commerce Backend!");
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello route working!" });
 });
 
-mongoose
-  .connect(
-    "mongodb+srv://vikaskumar20012001:vikas1234@demo-wd-8.sdcsiw7.mongodb.net/E-commerce"
-  )
-  .then(() => {
-    console.log("Connected to MongoDB successfully");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
-
-server.use("/auth", router);
-
-export default server;
+export default app;
